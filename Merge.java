d28e3849db1b74d2739ab07a02dddaa52e4093ce
rsarnath <136544306+rsarnath@users.noreply.github.com>
2024-08-25 20:28:36 -0500

    Update Merge.java

--- Merge.java
+++ Merge.java
@@ -1,65 +1,47 @@
 import java.io.*;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 import java.awt.*;
 import javax.swing.*;
 public class Merge {
-	public static void main(String[] args) throws FileNotFoundException {
-		int item1 = 0, item2 = 0;
-		String fileName1, fileName2, outFileName;
-		File dataFile1, dataFile2, outFile;
-		boolean hasNext1 = false, hasNext2 = false;
-		System.out.println("Jframe not created");
-		JFrame myframe = new JFrame(); // the lines that creates trouble
-		myframe.setSize(300, 300);
-		System.out.println("Jframe created");
-		JPanel buttonPanel = new JPanel();
+    public static void main(String[] args) throws FileNotFoundException {
+	int item1 = 0, item2 = 0;
+	String fileName1, fileName2, outFileName;
+	File dataFile1, dataFile2, outFile;
+	boolean hasNext1 = false, hasNext2 = false;
+	System.out.println("Jframe not created");
+	/* JFrame myframe = new JFrame(); // the lines that creates trouble
+	myframe.setSize(300, 300);
+	System.out.println("Jframe created");
+	JPanel buttonPanel = new JPanel();
         Container contentpane = myframe.getContentPane();
         contentpane.add(buttonPanel, "North");
         buttonPanel.add(new JButton("mybutton"));
-		myframe.setVisible(true); // goes with line above
-		myframe.repaint();
-		Scanner keyScanner = new Scanner(System.in);
-		System.out.println("Please enter the first file name: ");
-		fileName1 = keyScanner.nextLine();
-		System.out.println("Please enter the second file name: ");
-		fileName2 = keyScanner.nextLine();
-		System.out.println("Please enter the output file name: ");
-		outFileName = keyScanner.nextLine();		
-		dataFile1 = new File(fileName1);
-		dataFile2 = new File(fileName2);
-		outFile = new File(outFileName);
-		Scanner fileScanner1 = new Scanner(dataFile1);
-		Scanner fileScanner2 = new Scanner(dataFile2);
-		PrintWriter printer = new PrintWriter(outFile);
-		if (fileScanner1.hasNext()) {
-			hasNext1 = true; 
-			item1 = fileScanner1.nextInt();
-		} 
-		if (fileScanner2.hasNext()) {
-			hasNext2 = true; 
-			item2 = fileScanner2.nextInt();
-		}
-		while (hasNext1 && hasNext2) {
-			if (item1 < item2) {
-				printer.print(item1);
-				printer.print(' ');
-				if (fileScanner1.hasNext()) {
-					item1 = fileScanner1.nextInt();
-				} else {
-					hasNext1 = false;
-				}
-			} else {
-				printer.print(item2);
-				printer.print(' ');
-				if (fileScanner2.hasNext()) {
-					item2 = fileScanner2.nextInt();
-				} else {
-					hasNext2 = false;
-				}
-			}
-		}
-		while (hasNext1) {
+	myframe.setVisible(true); // goes with line above
+	myframe.repaint();
+	Scanner keyScanner = new Scanner(System.in);
+	System.out.println("Please enter the first file name: ");
+	fileName1 = keyScanner.nextLine();
+	System.out.println("Please enter the second file name: ");
+	fileName2 = keyScanner.nextLine();
+	System.out.println("Please enter the output file name: ");
+	outFileName = keyScanner.nextLine();		
+	dataFile1 = new File(fileName1);
+	dataFile2 = new File(fileName2);
+	outFile = new File(outFileName);
+	Scanner fileScanner1 = new Scanner(dataFile1);
+	Scanner fileScanner2 = new Scanner(dataFile2);
+	PrintWriter printer = new PrintWriter(outFile);
+	if (fileScanner1.hasNext()) {
+		hasNext1 = true; 
+		item1 = fileScanner1.nextInt();
+	} 
+	if (fileScanner2.hasNext()) {
+		hasNext2 = true; 
+		item2 = fileScanner2.nextInt();
+	}
+	while (hasNext1 && hasNext2) {
+		if (item1 < item2) {
 			printer.print(item1);
 			printer.print(' ');
 			if (fileScanner1.hasNext()) {
@@ -67,8 +49,7 @@ public class Merge {
 			} else {
 				hasNext1 = false;
 			}
-		}
-		while (hasNext2) {
+		} else {
 			printer.print(item2);
 			printer.print(' ');
 			if (fileScanner2.hasNext()) {
@@ -77,10 +58,29 @@ public class Merge {
 				hasNext2 = false;
 			}
 		}
-		printer.close();
-		fileScanner1.close();
-		fileScanner2.close();
-		keyScanner.close();
-		
 	}
+	while (hasNext1) {
+		printer.print(item1);
+		printer.print(' ');
+		if (fileScanner1.hasNext()) {
+			item1 = fileScanner1.nextInt();
+		} else {
+			hasNext1 = false;
+		}
+	}
+	while (hasNext2) {
+		printer.print(item2);
+		printer.print(' ');
+		if (fileScanner2.hasNext()) {
+			item2 = fileScanner2.nextInt();
+		} else {
+			hasNext2 = false;
+		}
+	}
+	printer.close();
+	fileScanner1.close();
+	fileScanner2.close();
+	keyScanner.close();
+	
+    }
 }
